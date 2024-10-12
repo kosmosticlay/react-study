5be3b08eb07b4ebe374d9841855c5ad298062da4
kosmosticlay <kosmosticlay@gmail.com>
2024-10-12 22:10:23 +0900

    refactor: Update structure for dynamic content rendering

--- custom-hooks/src/App.tsx
+++ custom-hooks/src/App.tsx
@@ -1,37 +1,20 @@
 import "./App.css";
-import useInput from "./hooks/useInput";
+import { useState } from "react";
+import UseInput from "./components/useInput";
 
 function App() {
-  const isValidLength = (value: string) => value.length < 15;
-  const isValidEmail = (value: string) => value.includes("@");
+  const [hook, setHook] = useState<string | null>(null);
 
-  const { value: name, handleInput: handleName } = useInput("", isValidLength);
-  const { value: email, handleInput: handleEmail } = useInput("", () => true); // 유효성 검사 없이 입력
-
-  const validateEmailOnBlur = () => {
-    if (!isValidEmail(email)) {
-      alert("유효하지 않은 이메일 주소입니다.");
-    }
+  const handleHook = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setHook(event.currentTarget.value);
   };
 
   return (
     <>
-      <h1>working? </h1>
-      <input
-        id="name"
-        placeholder="이름을 입력하세요."
-        value={name}
-        type="text"
-        onChange={handleName}
-      ></input>
-      <input
-        id="email"
-        placeholder="이메일을 입력하세요."
-        value={email}
-        type="email"
-        onChange={handleEmail}
-        onBlur={validateEmailOnBlur} // 포커스 벗어날 때 유효성 검사
-      ></input>
+      <button onClick={handleHook} value="useInput">
+        useInput
+      </button>
+      {hook === "useInput" && <UseInput />}
     </>
   );
 }
---
+++ custom-hooks/src/components/UseInput.tsx
@@ -0,0 +1,35 @@
+import useInput from "../hooks/useInput";
+
+export default function UseInput() {
+  const isValidLength = (value: string) => value.length < 15;
+  const isValidEmail = (value: string) => value.includes("@");
+
+  const { value: name, handleInput: handleName } = useInput("", isValidLength);
+  const { value: email, handleInput: handleEmail } = useInput("", () => true); // 유효성 검사 없이 입력
+
+  const validateEmailOnBlur = () => {
+    if (!isValidEmail(email)) {
+      alert("유효하지 않은 이메일 주소입니다.");
+    }
+  };
+  return (
+    <>
+      <h1>useInput </h1>
+      <input
+        id="name"
+        placeholder="이름을 입력하세요."
+        value={name}
+        type="text"
+        onChange={handleName}
+      ></input>
+      <input
+        id="email"
+        placeholder="이메일을 입력하세요."
+        value={email}
+        type="email"
+        onChange={handleEmail}
+        onBlur={validateEmailOnBlur} // 포커스 벗어날 때 유효성 검사
+      ></input>
+    </>
+  );
+}
